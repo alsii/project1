194d75b3a721ec77ec4c5e4d07125ca68cba2a96
Anna Pupysheva <anna-hse@ip-172-31-11-157.eu-central-1.compute.internal>
2015-04-15 13:01:13 +0200

    Added many methods to Kernel class

--- src/App/Kernel/Kernel.php
+++ src/App/Kernel/Kernel.php
@@ -1,13 +1,43 @@
 <?php
 namespace App\kernel;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 class Kernel {
 
-  public function init()  {
+  private $templateEngine;
+
+  public function init()
+  {
+    $this->initTemplateEngine();
   }
 
-  public function render()  {
+  public function render()
+  {
     echo "Hello, world!";
   }
-
+  
+  public function getBasePath()
+  {
+    return __DIR__.'/../../..';
+  }
+  
+  private function initTemplateEngine()
+  {
+    // Это loader будет искать шаблоны там, где мы указали при его создании
+    $loader = new Twig_Loader_Filesystem($this->getBasePath() . '/Resources/view');
+    
+    // создаем экземпляр шаблонизатора, передаем ему загрузчик шаблонов и массив параметров
+    // пока там один параметр - место для кеша
+    // сохраняем этот экземпляр в приватное свойство templateEngine
+    $this->templateEngine = new Twig_Environment($loader, array(
+        'cache' => $this->getBasePath() . '/cache/twig',
+    ));
+  }
+  
+  // Возвращает экземпляр шаблонизатора. Вдруг нам он снаружи понадобится ;)
+  private function getTemplateEngine()
+  {
+    return $this->templateEngine();
+  }
 }
