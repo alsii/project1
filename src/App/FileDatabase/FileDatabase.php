b03a754d4888a51fe6e2fdce26af3ad6e2877bda
alsii <alsii@users.noreply.github.com>
2015-04-21 10:28:52 +0200

    Update FileDatabase.php

--- src/App/FileDatabase/FileDatabase.php
+++ src/App/FileDatabase/FileDatabase.php
@@ -5,13 +5,13 @@ class FileDatabase inplements DatabaseInterface
 {
     /**
      * @var \App\Kernel\Kernel
-     * /
+     */
     private $kernel;
     
     /**
      * Save $entity into file
      * @param mixed $entity
-     * /
+     */
     public function save($entity);
     {
         $filename = sha1(microtime());
