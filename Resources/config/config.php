58220c842201f1902c85b96506df25cd3b0b0d57
alsii <alsii@users.noreply.github.com>
2015-04-21 12:09:41 +0200

    Update config.php

--- Resources/config/config.php
+++ Resources/config/config.php
@@ -1,10 +1,13 @@
 <?php
 return array(
- 'routing' => array(
-    array('pattern' => '/', 'controller' => 'Main', 'action' => 'index'),
-    array('pattern' => '/guestbook', 'controller' => 'Guestbook', 'action' => 'list'),
-    array('pattern' => '/guestbook/add', 'controller' => 'Guestbook', 'action' => 'add'),
-    array('pattern' => '/guestbook/:id/delete', 'controller' => 'Guestbook', 'action' => 'delete'),
-    array('pattern' => '/req/:text', 'controller' => 'Req', 'action' => 'print'),
- )
+    'routing' => array(
+        array('pattern' => '/', 'controller' => 'Main', 'action' => 'index'),
+        array('pattern' => '/guestbook', 'controller' => 'Guestbook', 'action' => 'list'),
+        array('pattern' => '/guestbook/add', 'controller' => 'Guestbook', 'action' => 'add'),
+        array('pattern' => '/guestbook/:id/delete', 'controller' => 'Guestbook', 'action' => 'delete'),
+        array('pattern' => '/req/:text', 'controller' => 'Req', 'action' => 'print'),
+    ),
+    'entities' => array(
+        array('entity' = 'App\Entity\Message', 'repository' => 'App\Entity\MessageRepository'),
+    )
 );
