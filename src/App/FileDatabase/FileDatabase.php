c377d2b203d4ed53a301fc8dbbaaf670dbb5749d
alsii <alsii@users.noreply.github.com>
2015-04-21 10:28:25 +0200

    Update FileDatabase.php

--- src/App/FileDatabase/FileDatabase.php
+++ src/App/FileDatabase/FileDatabase.php
@@ -1,9 +1,23 @@
+<?php
+namespace App/FileDatabase;
+
 class FileDatabase inplements DatabaseInterface
 {
+    /**
+     * @var \App\Kernel\Kernel
+     * /
+    private $kernel;
+    
+    /**
+     * Save $entity into file
+     * @param mixed $entity
+     * /
     public function save($entity);
     {
-        $time=sha1(microtime());
-        $text=$this->serialize($entity);
+        $filename = sha1(microtime());
+        $filedata = serialize($entity);
         $class = get_class_name($entity);
+        
+        $filepath = $kernel->getBasePath() . 'Database/' . $class . '/'; 
     }
 }
