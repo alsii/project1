9b222ebe8d3a8b1687f8ad24fbd10faa969ef0f9
Anna Pupysheva <anna-hse@ip-172-31-11-157.eu-central-1.compute.internal>
2015-05-18 09:46:00 +0200

    README.md, new tests

--- tests/App/Kernel/KernelTest.php
+++ tests/App/Kernel/KernelTest.php
@@ -8,10 +8,12 @@ class KernelTest extends PHPUnit_Framework_TestCase
     public function testRedirect()
     {
       $kernel = new Kernel();
-      $response = $kernel->redirect('http://test.url');
 
+      $response = $kernel->redirect('http://test.url');
       $this->assertInstanceOf('App\Kernel\Http\HtmlResponse', $response);
       $this->assertAttributeEquals(302, 'httpResponseCode', $response);
+      $this->assertAttributeEquals(array('Location: http://test.url'), 'headers', $response);
+      
     }
 
 }
