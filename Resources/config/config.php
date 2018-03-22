51265f2bd74caeb6fbe505c60d53586cd1c6ebcf
Aleksei Sitnikov <alsi.0669@gmail.com>
2018-03-22 12:21:51 +0100

    ParserTest::testParse added

--- Resources/config/config.php
+++ Resources/config/config.php
@@ -3,15 +3,15 @@ return array(
     'routing' => array(
         array('name' => 'main_index', 'pattern' => '/', 'controller' => 'Main', 'action' => 'index'),
         array('name' => 'main_beton', 'pattern' => '/beton', 'controller' => 'Main','action' => 'beton'),
-	array('name' => 'main_about', 'pattern' => '/about', 'controller' => 'Main','action' => 'about'),
-	array('name' => 'main_contacts', 'pattern' => '/contacts', 'controller' => 'Main','action' => 'contacts'),
-	array('name' => 'main_price', 'pattern' => '/price', 'controller' => 'Main','action' => 'price'),
+        array('name' => 'main_about', 'pattern' => '/about', 'controller' => 'Main','action' => 'about'),
+        array('name' => 'main_contacts', 'pattern' => '/contacts', 'controller' => 'Main','action' => 'contacts'),
+        array('name' => 'main_price', 'pattern' => '/price', 'controller' => 'Main','action' => 'price'),
         array('name' => 'guestbook_list', 'pattern' => '/guestbook', 'controller' => 'Guestbook', 'action' => 'list'),
         array('name' => 'guestbook_add', 'pattern' => '/guestbook/add', 'controller' => 'Guestbook', 'action' => 'add'),
         array('name' => 'guestbook_delete', 'pattern' => '/guestbook/:id/delete', 'controller' => 'Guestbook', 'action' => 'delete'),
         array('name' => 'req_print', 'pattern' => '/req/:text', 'controller' => 'Req', 'action' => 'print'),
     ),
     'entities' => array(
-        'App\Entity\Message' => array ('repository' => 'App\Entity\MessageRepository'),
+        'App\Entity\Message' => array('repository' => 'App\Entity\MessageRepository'),
     )
 );
--- tests/App/Kernel/ParserTest.php
+++ tests/App/Kernel/ParserTest.php
@@ -5,6 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 final class ParserTest extends TestCase
 {
+    const ROUTES = [
+        ['name' => 'test1', 'pattern' => '/test1', 'controller' => 'Test', 'action' => 'test1'],
+        ['name' => 'test2', 'pattern' => '/test2', 'controller' => 'Test', 'action' => 'test2'],
+        ['name' => 'order-list', 'pattern' => '/order', 'controller' => 'Order', 'action' => 'list'],
+        ['name' => 'order-new', 'pattern' => '/order/new', 'controller' => 'Order', 'action' => 'create'],
+        ['name' => 'order-show', 'pattern' => '/order/:id', 'controller' => 'Order', 'action' => 'show'],
+        ['name' => 'order-edit', 'pattern' => '/order/:id/edit', 'controller' => 'Order', 'action' => 'edit'],
+        ['name' => 'order-remove', 'pattern' => '/order/:id/remove', 'controller' => 'Order', 'action' => 'remove'],
+        ['name' => 'order-item-list', 'pattern' => '/order/:id/item', 'controller' => 'Order', 'action' => 'listItem'],
+        ['name' => 'order-item-show', 'pattern' => '/order/:order-id/item/:item-id', 'controller' => 'Order', 'action' => 'showItem'],
+    ];
+
    /**
     * @dataProvider tokenizeProvider
     */
@@ -43,4 +55,37 @@ final class ParserTest extends TestCase
             [['order' => 'value', ':id' => 'name', ':action' => 'name']],
         ];
     }
+    
+   /**
+    * @dataProvider parseProvider
+    */
+    public function testParse($pattern, $result)
+    {
+        $parser = new Parser(self::ROUTES);
+        $r = $parser->parse($pattern);
+        $this->assertEquals($r, $result);
+    }
+    
+    public function parseProvider()
+    {
+        return [
+            ['/test', false],
+            ['/test1', ['controller' => 'TestController', 'action' => 'test1Action', 'parameters' =>[]]],
+            ['/test2', ['controller' => 'TestController', 'action' => 'test2Action', 'parameters' =>[]]],
+            ['/order', ['controller' => 'OrderController', 'action' => 'listAction', 'parameters' =>[]]],
+            ['/order/new', ['controller' => 'OrderController', 'action' => 'createAction', 'parameters' =>[]]],
+            ['/order/42', ['controller' => 'OrderController', 'action' => 'showAction', 'parameters' =>['id' => 42]]],
+            ['/order/42/remove', ['controller' => 'OrderController', 'action' => 'removeAction', 'parameters' =>['id' => 42]]],
+            ['/order/42/item', ['controller' => 'OrderController', 'action' => 'listItemAction', 'parameters' =>['id' => 42]]],
+            ['/order/42/item-list', false],
+            ['/order/42/item/21', ['controller' => 'OrderController', 'action' => 'showItemAction', 'parameters' =>['order-id' => 42, 'item-id' => 21]]],
+            ['/order/42/item/21/delete', false],
+        ];
+    }
 }
+/*
+        ['name' => 'order-remove', 'pattern' => '/order/:id/remove', 'controller' => 'Order', 'action' => 'remove'],
+        ['name' => 'order-item-list', 'pattern' => '/order/:id/item', 'controller' => 'Order', 'action' => 'listItem'],
+        ['name' => 'order-item-show', 'pattern' => '/order/:order-id/item/:item-id', 'controller' => 'Order', 'action' => 'showItem'],
+*/
+
